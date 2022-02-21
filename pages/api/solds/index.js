ff791fee9638d26e3790e3ad50f8fb2376a4f46e
dougbica <dougbrasil.bica@gmail.com>
2022-02-21 08:54:39 -0300

    ajuste query

--- pages/api/solds/index.js
+++ pages/api/solds/index.js
@@ -7,9 +7,13 @@ export default async ( params, resp) => {
   const {start_date, end_date} = query;
   if (method === 'GET'){
     if (start_date && end_date){
-      const sql = `select u.id, u.name, P.release_at from  "User" u inner join "Purchase" P on u.id = P."userId"
-      inner join "Purchase_Product" PP on P.id = PP.purchase_id inner join "Product" P2 on P2.id = PP.product_id      
-      where P2.name like '%Leite%' and P.release_at between '${start_date}' and '${end_date}' GROUP BY u.id, u.name, P.release_at HAVING SUM(P2.price) > 30`  
+      const sql = `Select u.id, u.name, P.release_at from  "User" u inner join "Purchase" P on u.id = P."userId"
+      inner join "Purchase_Product" PP on P.id = PP.purchase_id inner join "Product" P2 on PP.product_id = P2.id
+      where P.id in (
+          select P.purchase_id
+          from "Purchase_Product" P inner join "Product" P2 on P2.id = P.product_id
+          group by P.purchase_id Having SUM(P2.price) > 30 and count(P.product_id) >= 3
+      ) and P.release_at between '${start_date}' and '${end_date}' and P2.name like '%Leite%' group by u.id, u.name, P.release_at`;
       const res = await prisma.$queryRawUnsafe(`${sql}`);
       resp.status(200).json(res);
     } else {
